b02592763ded9ec57bf82fa8833f8644b49d1c5d
utkarshjayaswal <utkarsh.jayaswal@outlook.com>
2022-01-25 09:58:31 +0530

    made changes and resolved bugs

--- fb_automation.py
+++ fb_automation.py
@@ -1,22 +1,23 @@
 from selenium import webdriver
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
-usrname=input('Enter Your Username:')
-password=input('Enter Your Password:')
+
+username = input('Enter Your Username:')  # Input your email address of facebook in the terminal
+password = input('Enter Your Password:')  # Input your password of facebook in the terminal
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.facebook.com/')
-print ("Opened facebook")
+print("Opened facebook")
 sleep(2)
 username_box = driver.find_element_by_id('email')
-username_box.send_keys(usrname)
-print ("Your user name has been entered")
+username_box.send_keys(username)
+print("Your user name has been entered")
 sleep(1)
 password_box = driver.find_element_by_id('pass')
 password_box.send_keys(password)
-print ("you password has been entered")
+print("you password has been entered")
 login_box = driver.find_element_by_name('login')
 login_box.click()
-print ("Done")
-input('You can type quit to exist')
+print("Done")
+input('You can type quit to exit from the program')
 driver.quit()
 print("Finished")
